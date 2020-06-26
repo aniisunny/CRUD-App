7523c78ee99014ad7a092b38c30982b0fd648808
Anirrudha <aniruddhaagrawal455@gmail.com>
2020-06-27 03:25:06 +0530

    add sort feature

--- app/src/main/java/com/example/crudapp/AddItem.java
+++ app/src/main/java/com/example/crudapp/AddItem.java
@@ -1,7 +1,12 @@
 package com.example.crudapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddItem extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class AddItem extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        final EditText nameEditText = (EditText) findViewById(R.id.newName);
+        final EditText departmentEditText = (EditText) findViewById(R.id.newDepartment);
+        final EditText collegeEditText = (EditText) findViewById(R.id.newCollege);
+        final Button addItemButton = (Button) findViewById(R.id.addItem);
+
+        addItemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nameString = nameEditText.getText().toString();
+                String departmentString = departmentEditText.getText().toString();
+                String collegeString = collegeEditText.getText().toString();
+
+                MainActivity.arrayOfStudents.add (new Student(nameString, departmentString, collegeString));
+
+                Intent intent = new Intent (AddItem.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/crudapp/MainActivity.java
+++ app/src/main/java/com/example/crudapp/MainActivity.java
@@ -2,32 +2,68 @@ package com.example.crudapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class MainActivity extends AppCompatActivity {
 
-    public ArrayList<Student> arrayOfStudents;
+    static public ArrayList<Student> arrayOfStudents = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        arrayOfStudents = new ArrayList<>();
-
-        arrayOfStudents.add(new Student("Aniruddha", "CSE", "Chandigarh Univeristy"));
+        arrayOfStudents.add(new Student("Aniruddha", "CSE", "Chandigarh University"));
         arrayOfStudents.add(new Student("Divya Ahuja", "CSE", "Banasthali Vidyapith"));
-        arrayOfStudents.add(new Student("Rahul Bansal", "CSE", "Chandigarh Univeristy"));
-        arrayOfStudents.add(new Student("Riddhi Sharma", "ECE", "UPTU"));
-        arrayOfStudents.add(new Student("Simran Kaur", "CSE", "Chandigarh Univeristy"));
+        arrayOfStudents.add(new Student("Rahul Bansal", "CSE", "Chandigarh University"));
+        arrayOfStudents.add(new Student("Riddhi Sharma", "ECE", "AKEG"));
+        arrayOfStudents.add(new Student("Simran Kaur", "CSE", "Chandigarh University"));
         arrayOfStudents.add(new Student("Charu Garg", "CSE", "Banasthali Vidyapith"));
 
-        StudentAdapter adapter = new StudentAdapter (this, arrayOfStudents);
+        final StudentAdapter adapter = new StudentAdapter (this, arrayOfStudents);
 
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(adapter);
+
+        Button addButton = (Button) findViewById(R.id.addItem);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (MainActivity.this, AddItem.class);
+                startActivity(intent);
+            }
+        });
+
+//        Button removeButton = (Button) findViewById(R.id.removeItem);
+//        removeButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //adapter.remove(0);
+//                adapter.notifyDataSetChanged();
+//            }
+//        });
+
+        Button sortButton = (Button) findViewById(R.id.sortByName);
+        sortButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Collections.sort(arrayOfStudents, new Comparator<Student>() {
+                    @Override
+                    public int compare(Student o1, Student o2) {
+                        return o1.name.compareTo(o2.name);
+                    }
+                });
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
