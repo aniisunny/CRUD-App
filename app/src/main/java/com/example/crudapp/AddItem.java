8749112c7ed0644a4675ef58106b846cdec44937
Anirrudha <aniruddhaagrawal455@gmail.com>
2020-06-27 02:01:05 +0530

    Add Item Features

---
+++ app/src/main/java/com/example/crudapp/AddItem.java
@@ -0,0 +1,13 @@
+package com.example.crudapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+    }
+}
